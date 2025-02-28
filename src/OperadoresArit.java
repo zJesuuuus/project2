51c4119fb2d47d11bf0521f9108a834e14f0bd90
zJesuuuus <jhurquijoa@ufpso.edu.co>
2025-02-28 17:49:05 -0500

    Se agrega ejercicio operadores Arit

---
+++ src/OperadoresArit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OperadoresArit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        int numero1 = scanner.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int numero2 = scanner.nextInt();
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
+        int modulo = numero1 % numero2;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Módulo: " + modulo);
+    }
+}

99a80b9a669e8f1f70f700743349683e716a72cc
zJesuuuus <jhurquijoa@ufpso.edu.co>
2025-03-01 17:46:22 -0500

    Sistema banco ejercicio 2

--- src/Banco.java
+++ src/Banco.java
@@ -1,6 +1,19 @@
 public class Banco {
     public static void main(String[] args) {
         int dineroInicial = 1000;
-        
+        int dineroRetiro = 200;
+        System.out.println("Saldo Inicial: " + dineroInicial);
+        System.out.println("Has retirado: " + dineroRetiro);
+        int dineroSemna1 = dineroInicial - dineroRetiro;
+        System.out.println("Saldo Restante: " + dineroSemna1);
+        System.out.println("Has retirado: " + dineroRetiro);
+        int dineroSemna2 = dineroSemna1 - dineroRetiro;
+        System.out.println("Saldo Restante: " + dineroSemna2);
+        System.out.println("Has retirado: " + dineroRetiro);
+        int dineroSemna3 = dineroSemna2 - dineroRetiro;
+        System.out.println("Saldo Restante: " + dineroSemna3);
+        System.out.println("Has retirado: " + dineroRetiro);
+        int dineroSemna4 = dineroSemna3 - dineroRetiro;
+        System.out.println("Ya es final de mes, tu saldo restante es: " + dineroSemna4);
     }
 }

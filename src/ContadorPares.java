9afb0843adca3b4335708db4b68c948858406265
zJesuuuus <jhurquijoa@ufpso.edu.co>
2025-03-07 22:21:58 -0500

    Se sube ejercicio contador de pares

---
+++ src/ContadorPares.java
@@ -0,0 +1,12 @@
+public class ContadorPares {
+    public static void main(String[] args) {
+        int contador = 0;
+        for (int i =0; i < 100; i++){
+            int div = (i % 2);
+            if (div == 0) {
+                contador++;
+            }
+        }
+        System.out.println("La cantidad de números pares del 1 - 100 son: " + contador);
+    }
+}

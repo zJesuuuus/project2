0305475a61d1e3eff81e3d0e6c94e605e0761d87
zJesuuuus <jhurquijoa@ufpso.edu.co>
2025-03-21 22:01:07 -0500

    Se sube el ejercicio diez

---
+++ src/EjercicioDiez.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class EjercicioDiez {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese sueldo: ");
+        float sueldo = input.nextFloat();
+        System.out.println("Ingrese años de antiguedad: ");
+        float añosAntiguedad = input.nextFloat();
+        
+        
+        if (sueldo < 500 && añosAntiguedad >= 10) {
+            double sueldoTotal = (sueldo * 0.20 + sueldo);
+            int total = (int) sueldoTotal;
+            System.out.println(total);
+            input.close();
+            return;
+        }else if (sueldo < 500 && añosAntiguedad < 10) {
+            double sueldoTotal = (sueldo * 0.05 + sueldo);
+            int total = (int) sueldoTotal;
+            System.out.println(total);
+            input.close();
+            return;
+        }else if (sueldo >= 500 ) {
+            System.out.println(sueldo);
+            input.close();
+            return;
+        }
+        input.close();
+    }
+}

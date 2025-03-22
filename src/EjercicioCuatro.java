cba547499c54403de9f38dbc417c30f3969bf615
zJesuuuus <jhurquijoa@ufpso.edu.co>
2025-03-21 21:58:59 -0500

    Se sube el ejercicio cuatro

---
+++ src/EjercicioCuatro.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EjercicioCuatro {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese el sueldo: ");
+        float sueldo = input.nextFloat();
+        
+        if (sueldo > 3000) {
+            System.out.println("Debes abonar impuestos.");
+            input.close();
+            return;
+        }else{
+            System.out.println("No debes abonar impuestos Crack.");
+            input.close();
+            return;
+        }
+    }
+}

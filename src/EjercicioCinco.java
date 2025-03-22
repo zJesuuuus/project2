19ad2c33158336e34c0331036ac10733fcab81ca
zJesuuuus <jhurquijoa@ufpso.edu.co>
2025-03-21 21:59:07 -0500

    Se sube el ejercicio cinco

---
+++ src/EjercicioCinco.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EjercicioCinco {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero: ");
+        byte num1 = input.nextByte();
+        System.out.println("Ingrese el segundo numero: ");
+        byte num2 = input.nextByte();
+        if (num1 > num2) {
+            System.out.println("El numero mayor es: " + num1);
+            input.close();
+            return;
+        }else if (num1 < num2) {
+            System.out.println("El numero mayor es: " + num2);
+            input.close();
+            return;
+        }else{
+            System.out.println("Los numeros son iguales");
+            input.close();
+            return;
+        }
+    }
+}
